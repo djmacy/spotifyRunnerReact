d2be7e5803b39cbbbac02942144436b22b07ec1b
David Macy <dmacy@carroll.edu>
2024-11-08 19:58:31 -0700

    changes to the header. added cards that the user can select premade queues

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import './Header.css';
 import logo from '../images/spotifyRunnerLogo.png';
-
+import cardImage from '../images/card1.png';
+import cardImage2 from '../images/card2.png';
+import cardImage3 from '../images/card3.png';
+import cardImage4 from '../images/card4.png';
+import cardImage5 from '../images/card5.png';
 
 const Header = () => {
     return (
@@ -19,8 +23,8 @@ const Header = () => {
                 <div className="navbar-right">
                     <a href="#discover">Discover</a>
                     <a href="#playlists">Playlists</a>
-                    <a href="#profile">Profile</a>
-                    <a href="#settings">Settings</a>
+                    <a href="#profile">Liked Songs</a>
+                    <a href="#settings">Login</a>
                 </div>
             </div>
             <div className="banner">
@@ -28,6 +32,31 @@ const Header = () => {
                     <h1 className="text-run">RUN</h1>
                     <h2 className="text-to-the">TO THE</h2>
                     <h1 className="text-beat">BEAT</h1>
+                </div>
+            </div>
+
+            <div className="card-container">
+                <div className="card" style={{backgroundImage: `url(${cardImage}`}}>
+
+                    <h3 className="card-title">HIP HOP</h3>
+                    <button className="preview-button">Preview</button>
+                </div>
+                <div className="card" style={{backgroundImage: `url(${cardImage2}`}}>
+
+                    <h3 className="card-title">LATINO</h3>
+                    <button className="preview-button">Preview</button>
+                </div>
+                <div className="card" style={{backgroundImage: `url(${cardImage3}`}}>
+                    <h3 className="card-title">POP</h3>
+                    <button className="preview-button">Preview</button>
+                </div>
+                <div className="card" style={{backgroundImage: `url(${cardImage4}`}}>
+                    <h3 className="card-title">ROCK</h3>
+                    <button className="preview-button">Preview</button>
+                </div>
+                <div className="card" style={{backgroundImage: `url(${cardImage5}`}}>
+                    <h3 className="card-title">TECHNO</h3>
+                    <button className="preview-button">Preview</button>
                 </div>
             </div>
         </header>
